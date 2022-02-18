5fa0dca55abf6518b9d13a4fbada873449db9c74
Shailseen <shailesh13246@gmail.com>
2022-02-18 23:39:17 +0530

    Homepage

---
+++ pages/homepage/homepage.js
@@ -0,0 +1,25 @@
+// var slideIndex = 0;
+// var slideIndex1 = 0;
+// carousel();
+// // console.log("hiii");
+// function carousel() {
+//   var i;
+//   var x = document.getElementsByClassName("poster-image");
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > x.length) {slideIndex = 1}
+//   x[slideIndex-1].style.display = "block";
+//   setTimeout(carousel, 2000); // Change image every 2 seconds
+
+
+// //   var y = document.getElementsByClassName("text-container1");
+// //   for (i = 0; i < y.length; i++) {
+// //     y[i].style.display = "none";
+// //   }
+// //   slideIndex1++;
+// //   if (slideIndex1 > y.length) {slideIndex1 = 1}
+// //   y[slideIndex1-1].style.display = "block";
+// //   setTimeout(carousel, 3000); // Change image every 2 seconds
+// }
